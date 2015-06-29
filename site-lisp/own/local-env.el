160f6de639b2e7d88aeef0ce00d5b6d717346a28
Ola Bini <ola.bini@gmail.com>
2015-06-29 08:45:58 -0500

    Update local env calculation to cache values per directory

--- site-lisp/own/local-env.el
+++ site-lisp/own/local-env.el
@@ -26,17 +26,33 @@
 
 (defvar local-env-shell-name "zsh" "shell to invoke to get current environment")
 
-;; Sets process-environment and exec-path as file local variables for this file.
-;; This function is suitable to add as a hook
-(defun set-local-envs ()
+(defconst local-env-cache (make-hash-table :test 'equal))
+
+(defun calculate-current-env ()
   (let* ((current-env (shell-command-to-string (concat local-env-shell-name " -lic '_direnv_hook && env'")))
          (each-env (split-string current-env "\n"))
          (env-split (mapcar (lambda (e) (split-string e "=")) each-env))
          (path (split-string (car (cdr (assoc "PATH" env-split))) ":")))
+    (list each-env path)))
+
+(defun get-or-cache-current-env (dir)
+  (let ((res (gethash dir local-env-cache)))
+    (if res
+        res
+        (let ((calc (calculate-current-env)))
+          (puthash dir calc local-env-cache)
+          calc))))
+
+;; Sets process-environment and exec-path as file local variables for this file.
+;; This function is suitable to add as a hook
+(defun set-local-envs ()
+  (let* ((res (get-or-cache-current-env default-directory))
+         (pe (car res))
+         (pa (car (cdr res))))
     (make-local-variable 'process-environment)
     (make-local-variable 'exec-path)
-    (setq process-environment each-env)
-    (setq exec-path path)))
+    (setq process-environment pe)
+    (setq exec-path pa)))
 
 (provide 'local-env)
 ;;; local-env.el ends here
